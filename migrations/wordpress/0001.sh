2bda1427275b0e3c5a3eb1a5f3e62c96203e751f
david-binda <david.binovec@gmail.com>
2016-09-17 23:07:35 +0000

    install MySQL only if it's not already running

--- migrations/wordpress/0001.sh
+++ migrations/wordpress/0001.sh
@@ -10,15 +10,18 @@ ln -sf /etc/mysql-initscripts/mysql-5.6 /etc/init.d/mysql1-0
 ln -sf /usr/local/mysql-latest /usr/local/mysql
 PATH=$PATH:/usr/local/mysql/bin/
 
-mkdir -p /var/log/mysql1-0
-chown mysql.mysql /var/log/mysql1-0
-cd /usr/local/mysql5.6/
-./scripts/mysql_install_db --defaults-file=/etc/mysql/mysql1-0.cnf --user=mysql --datadir=/var/lib/mysql1-0 --force  --skip-name-resolve
-chown -R mysql.mysql /var/lib/mysql1-0
-/etc/init.d/mysql1-0 start
-/usr/local/mysql/bin/mysqladmin --defaults-file=/etc/mysql/mysql1-0.cnf -u root password 'god'
-mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -pgod -e "create user 'wp'@'localhost' IDENTIFIED BY 'drupal';"
-mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -pgod -e "GRANT ALL ON wp.* TO 'wp'@'localhost';"
+if [[ ! "$(/etc/init.d/mysql1-0 status)" =~ "start/running" ]]
+then
+	mkdir -p /var/log/mysql1-0
+	chown mysql.mysql /var/log/mysql1-0
+	cd /usr/local/mysql5.6/
+	./scripts/mysql_install_db --defaults-file=/etc/mysql/mysql1-0.cnf --user=mysql --datadir=/var/lib/mysql1-0 --force  --skip-name-resolve
+	chown -R mysql.mysql /var/lib/mysql1-0
+	/etc/init.d/mysql1-0 start
+	/usr/local/mysql/bin/mysqladmin --defaults-file=/etc/mysql/mysql1-0.cnf -u root password 'god'
+	mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -pgod -e "create user 'wp'@'localhost' IDENTIFIED BY 'drupal';"
+	mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -pgod -e "GRANT ALL ON wp.* TO 'wp'@'localhost';"
+fi
 mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -SN  -pgod -e "DROP DATABASE wordpress;"
 mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -pgod -e "CREATE DATABASE wordpress;"
 
