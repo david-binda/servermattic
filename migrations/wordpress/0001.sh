af73838f214af1cc0207206fd8fdeee3c386bf6a
david-binda <david.binovec@gmail.com>
2016-09-17 20:04:08 +0000

    database install and setup script

--- migrations/wordpress/0001.sh
+++ migrations/wordpress/0001.sh
@@ -1,6 +1,21 @@
-# bin bash
+#!/bin/bash
 apt-get update
 apt-get install -fqqy subversion
 svn co https://core.svn.wordpress.org/tags/4.6.1/ /var/www/
 rm -Rf /var/www/.svn
 service nginx restart
+
+ln -sf /etc/mysql-initscripts/mysql-5.6 /etc/init.d/mysql1-0
+ln -sf /usr/local/mysql-latest /usr/local/mysql
+PATH=$PATH:/usr/local/mysql/bin/
+
+mkdir -p /var/log/mysql1-0
+chown mysql.mysql /var/log/mysql1-0
+cd /usr/local/mysql5.6/
+./scripts/mysql_install_db --defaults-file=/etc/mysql/mysql1-0.cnf --user=mysql --datadir=/var/lib/mysql1-0 --force  --skip-name-resolve
+chown -R mysql.mysql /var/lib/mysql1-0
+/etc/init.d/mysql1-0 start
+/usr/local/mysql/bin/mysqladmin --defaults-file=/etc/mysql/mysql1-0.cnf -u root password 'god'
+mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -pgod -e "create user 'wp'@'localhost' IDENTIFIED BY 'drupal';"
+mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -pgod -e "GRANT ALL ON wp.* TO 'wp'@'localhost';"
+mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -pgod -e "CREATE DATABASE wordpress;"
